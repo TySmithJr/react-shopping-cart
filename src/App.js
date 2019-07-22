9768d0dfcd90c22396aed7e9e2fe19967d3e3acc
Tyrone Smith <billionairefly.ts@gmail.com>
2019-07-22 18:55:11 -0400

    complete

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import { ProductContext } from "./components/contexts";
+import { CartContext } from "./components/contexts"
 
 function App() {
 	const [products] = useState(data);
@@ -17,26 +20,28 @@ function App() {
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			
+
+		  <ProductContext.Provider value={{ products, addItem }} > 
+
+		  	<CartContext.Provider value={{cart}}>
+				<Navigation  />
+			</CartContext.Provider>
+
 
 			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
-
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
+			<Route exact path="/" component={Products} />
+			
+		<CartContext.Provider value={{cart}}>
+			<Route path="/cart" component={ShoppingCart}
+		
+				
+			/> </CartContext.Provider>
+		  </ProductContext.Provider>
 		</div>
 	);
 }
 
 export default App;
+
+
